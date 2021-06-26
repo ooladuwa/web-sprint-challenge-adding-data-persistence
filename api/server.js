ba73054d5780a1cb86e304e76714b9d1a3c4555c
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-06-26 10:21:53 -0400

    created server.js and index.js and connected server to router files. Server success: runs on port 4000

--- api/server.js
+++ api/server.js
@@ -1,1 +1,20 @@
 // build your server here and require it from index.js
+const express = require("express");
+const helmet = require("helmet");
+
+const projectRouter = require("./project/router.js");
+const resourceRouter = require("./resource/router.js");
+const taskRouter = require("./task/router.js");
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use("/api", projectRouter);
+server.use("/api", resourceRouter);
+server.use("/api", taskRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "We up and running my dude!" });
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+const server = require("./api/server.js")
+
+const port = process.env.port || 4000
+
+server.listen(port, () => {
+  console.log(`Server is currently running on port ${port} my incredible friend!`)
+})
